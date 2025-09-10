958920ee86c6f9293366f8ad89b86b0054fbfda1
Diogo Martino <diogo.martino10@gmail.com>
2025-09-10 16:28:38 +0100

    chore: add contact email

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link';
 import { memo } from 'react';
 
+const Separator = () => (
+  <div className="hidden lg:block mx-2 h-3 border-l border-white/20" />
+);
+
 const Footer = memo(() => {
   return (
     <footer className="flex flex-col lg:flex-row items-center justify-center w-full py-3 mt-4">
@@ -8,13 +12,21 @@ const Footer = memo(() => {
         Todo o material é disponibilizado para fins educacionais e de
         preservação cultural, sem fins lucrativos
       </span>
-      <div className="mx-2 h-3 border-l border-white/20" />
+      <Separator />
       <Link
         href="https://github.com/diogomartino/osgatos"
         target="_blank"
         className="text-[10px] text-white/60 hover:text-white transition-colors"
       >
         Github
+      </Link>
+      <Separator />
+      <Link
+        href="mailto:geral@osgatos.net"
+        target="_blank"
+        className="text-[10px] text-white/60 hover:text-white transition-colors"
+      >
+        Contacto
       </Link>
     </footer>
   );
